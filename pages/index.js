6533a6a617336096899c306019b9d9240c182785
StrangeReunion <82770528+StrangeReunion@users.noreply.github.com>
2021-04-18 12:43:21 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,3 @@
-`import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        <a href="https://youtube.com">YouTube here</a>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
-`
 <BuilderCarousel
   class="builder-carousel"
   data-name="builder-carousel"
