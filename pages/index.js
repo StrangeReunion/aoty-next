047240b41cc21d37a8d3da909f43a18bbceffb85
StrangeReunion <82770528+StrangeReunion@users.noreply.github.com>
2021-04-18 12:53:32 -0400

    Update and rename index.html to index.js

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        <a href="https://youtube.com">YouTube here</a>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
